3a1341967ad1ab53f9696deb5f5951cd7af0c406
Sean Sarokas <sps4462@gmail.com>
2022-04-07 21:21:29 -0400

    commit tree 10638ebc4a2a27ef021bc63717fc1f0c93a8a68b

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+/*function multiply(num1, num2) {
+    let answer;
+    answer = num1 * num2;
+    return answer;
+}*/
+const num1 = 2;
+const num2 = 31;
+const num3 = 16;
+const num4 = 6;
+
+let multiply, random, mod, max;
+
+multiply = num1 * num2;
+
+random = Math.floor(Math.random() * 100);
+
+mod = num3 % num4;
+
+max = Math.max(1, 2, 3, 20);
